b41a3e018fa3a40ec265e06a22a4b3582d3e23aa
Owen <as05130502@gmail.com>
2023-12-18 19:53:06 +0800

    init

---
+++ demovideo/src/main/java/com/example/demovideo/DemovideoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demovideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemovideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemovideoApplication.class, args);
+    }
+
+}
---
+++ demovideo/src/test/java/com/example/demovideo/DemovideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demovideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemovideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
